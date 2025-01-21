c1e60e70ff7370e2ba9c38a8453133b99204ed7d
Yerzat <erzatnumnberone@gmail.com>
2025-01-21 10:36:12 +0500

    Test for connect with group

---
+++ src/data/PostgreDB.java
@@ -0,0 +1,12 @@
+package data;
+
+public class PostgreDB {
+    private String host;
+    private String username;
+    private String password;
+    private String DBname;
+
+
+    private Connec
+
+}
