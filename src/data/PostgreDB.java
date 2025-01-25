f58dcc862163cfa2d4c0c690e1778883b367e1c7
Admin <kundyz.mus@mail.ru>
2025-01-25 08:40:38 +0500

    degw3gwegt

--- src/data/PostgreDB.java
+++ src/data/PostgreDB.java
@@ -3,6 +3,7 @@ package data;
 import data.interfaces.IDB;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class PostgreDB implements IDB {
     private String host;
@@ -10,14 +11,75 @@ public class PostgreDB implements IDB {
     private String password;
     private String dbName;
 
+    private Connection connection;
 
+    public PostgreDB(String host, String username, String password, String dbName) {
+        setHost(host);
+        setUsername(username);
+        setPassword(password);
+        setDbName(dbName);
+    }
+
+
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+
+        this.password = password;
+    }
+
+    public String getHost() {
+
+        return host;
+    }
+
+    public void setHost(String host) {
+
+        this.host = host;
+    }
     @Override
     public Connection getConnection() {
+        String connectionUrl = host + "/" + dbName;
+        try {
+            if (connection == null && !connection.isClosed()) {
+                return connection;
+            }
+            Class.forName("org.postgresql.Driver");
+            connection=DriverManager.getConnection(connectionUrl,username,password);
+            return connection;
+        }catch (Exception e) {
+            System.out.println("Failed to connect to database: "+e.getMessage());
+
+        }
         return null;
+
     }
 
     @Override
     public void close() {
 
     }
+
+
+
+
 }
