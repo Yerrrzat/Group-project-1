dc6305a7a273a43ca991fa696a3b2aa4fcb46ab2
Dosh1K <ayazbaev.daniar@gmail.com>
2025-01-31 20:19:20 +0500

    Renamed from UserId to user_id and so on

--- src/controllers/interfaces/IReviewController.java
+++ src/controllers/interfaces/IReviewController.java
@@ -1,7 +1,7 @@
 package controllers.interfaces;
 
 public interface IReviewController {
-    String createReview(int userId, int deviceId, int rating, String comment);
+    String createReview(int user_id, int device_id, int rating, String comment);
     String getReviewById(int id);
     String getAllReviews();
 }
