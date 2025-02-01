f6549ac2752c9e3f16e1efb0ccc8fee5d27e0714
Yerzat <erzatnumnberone@gmail.com>
2025-02-01 08:01:42 +0500

    Yerzat

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import repositories.interfaces.*;
 
 public class Main {
     public static void main(String[] args) {
-        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432","postgres","123456789","dev_store");
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432","postgres","Qalzatov8745","db");
 
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
