3f722d3f85c40b657f985a2bf106bc28dd98d3cb
Yerzat <erzatnumnberone@gmail.com>
2025-02-08 07:20:42 +0500

    lambda

--- src/management/Main.java
+++ src/management/Main.java
@@ -9,7 +9,7 @@ import repositories.interfaces.*;
 
 public class Main {
     public static void main(String[] args) {
-        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432","postgres","Kundyz2007","postgres");
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432","postgres","Qalzatov8745","db");
 
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
